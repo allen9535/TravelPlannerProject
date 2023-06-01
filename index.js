be6f8d9b7ebe161770ded27fba60dc27fa1ecdad
allen9535 <allen9535@naver.com>
2023-06-01 11:42:23 +0900

    230601

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let url = 'https://estsoft-openai-api.jejucodingcamp.workers.dev/';
 let data = [{
     role: 'system',
-    content: 'assistant는 느긋한 여행을 추천하고, 날짜별로 여행 계획을 세워 Day1, Day2와 같은 방식으로 표시하며, 아주 상세한 답변을 주는 여행 전문가이다.'
+    content: 'assistant는 느긋한 여행을 추천하고, 날짜별로 여행 계획을 세워 Day1, Day2와 같은 방식으로 표시하며 아주 상세한 답변을 주는 여행 전문가이다.'
 }];
-let $place = document.querySelector('#place');
+let $startPoint = document.querySelector('#start-point');
+let $destination = document.querySelector('#destination');
 let $schedule = document.querySelector('#schedule');
 let $must = document.querySelector('#must-visit');
 let $rent = document.getElementsByName('car-rent');
 let $button = document.querySelector('button');
+let $loader = document.querySelector('#loader');
 
 
 function radioCheck() {
@@ -43,18 +45,29 @@ function makeQuestion() {
         .then(res => {
             document.querySelector('#announce').remove();
             document.querySelector('#contents').innerText = res.choices[0].message.content;
+            removeLoader();
         });
+}
+
+function appearLoader() {
+    $loader.style.display = 'block';
+}
+
+function removeLoader() {
+    $loader.style.display = 'none';
 }
 
 
 $button.addEventListener('click', e => {
     e.preventDefault();
+    appearLoader();
 
-    var userInputData = `나는 ${$place.value}로 여행을 가려고 해. ${$schedule.value} 정도 머물 예정이고, ${$must.value}는 꼭 들러보고 싶어. `;
+    var userInputData = `나는 ${$startPoint}에서 출발해 ${$destination.value}로 여행을 가려고 해. ${$schedule.value} 정도 머물 예정이고, ${$must.value}는 꼭 들러보고 싶어. `;
     if (radioCheck() === 'yes') {
-        userInputData += `그리고 차는 렌트하기로 했어.`
+        S
+        userInputData += `그리고 차량은 가져가기로 했어.`
     } else {
-        userInputData += `그리고 차는 렌트하지 않기로 했어.`
+        userInputData += `그리고 차량은 가져가지 않기로 했어.`
     }
 
     emptyValue();
